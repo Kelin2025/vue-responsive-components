110b45a49557b1cfc2f599ed8c15169a5ae69a02
Anton Kosykh <kelin2025@yandex.ru>
2018-04-22 20:54:37 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,69 @@
+import throttle from "lodash.throttle"
+import ResizeObserver from "resize-observer-polyfill"
+
+export const ResizeableMixin = {
+  data() {
+    return {
+      el: {
+        width: 0,
+        height: 0,
+        is: {}
+      }
+    }
+  },
+  mounted() {
+    if (
+      typeof process === "undefined" ||
+      (!process.server && (this.breakpoints || this.$options.breakpoints))
+    ) {
+      this.$nextTick(() => {
+        const handleResize = throttle(entries => {
+          const cr = entries[0].contentRect
+          ;(this.el.width = cr.width), (this.el.height = cr.height)
+          const conds = Object.assign(
+            {},
+            this.breakpoints || {},
+            this.$options.breakpoints || {}
+          )
+          for (const breakpoint in conds) {
+            this.$set(this.el.is, breakpoint, conds[breakpoint](this.el))
+          }
+        }, 200)
+
+        const observer = new ResizeObserver(handleResize)
+        if (this.$el instanceof Element) {
+          observer.observe(this.$el)
+        }
+      })
+    }
+  }
+}
+
+export const Resizeable = {
+  data() {
+    return { init: false }
+  },
+  props: {
+    noHide: { type: Boolean, default: false },
+    breakpoints: { type: Object, default: undefined }
+  },
+  mixins: [ResizeableMixin],
+  render(h) {
+    const slot =
+      (this.$scopedSlots.default && this.$scopedSlots.default(this.el)) ||
+      this.$slots.default
+
+    return !this.noHide && !this.init
+      ? h(
+          "div",
+          {
+            style: { visibility: "hidden" }
+          },
+          [slot]
+        )
+      : slot
+  },
+  mounted() {
+    this.init = true
+  }
+}
